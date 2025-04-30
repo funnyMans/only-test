ca37239825c7ea0a9df6453c9c57345c8dbcb2a6
Vladimir <vl.petrosyan94@gmail.com>
2025-04-30 16:39:03 +0400

    fix/still guessing

--- src/features/blog/historical-dates/index.tsx
+++ src/features/blog/historical-dates/index.tsx
@@ -1,8 +1,8 @@
 import HistoricalDatesContextProvider from './context/HistoricalDatesContext';
-import CategoriesAnimatedSpinner from './components/CategoriesAnimatedSpinner';
-import HistoricalDatesSlider from './components/HistoricalDatesSlider';
+// import CategoriesAnimatedSpinner from './components/CategoriesAnimatedSpinner';
+// import HistoricalDatesSlider from './components/HistoricalDatesSlider';
 import CurrentCategoryView from './components/CurrentCategoryView';
-import EventsDateRange from './components/EventsDateRange';
+// import EventsDateRange from './components/EventsDateRange';
 import DatesHeading from './components/DatesHeading';
 
 import { FlexContainerSC } from '@/shared/ui/containers/FlexContainer';
@@ -17,10 +17,10 @@ const HistoricalDatesArea = () => {
     >
       <DatesHeading />
       <HistoricalDatesContextProvider>
-        <CategoriesAnimatedSpinner controls withLabel />
-        <EventsDateRange />
+        {/* <CategoriesAnimatedSpinner controls withLabel /> */}
+        {/* <EventsDateRange /> */}
         <CurrentCategoryView />
-        <HistoricalDatesSlider />
+        {/* <HistoricalDatesSlider /> */}
       </HistoricalDatesContextProvider>
     </FlexContainerSC>
   );
