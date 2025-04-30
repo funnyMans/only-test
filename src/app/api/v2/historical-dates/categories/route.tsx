f51653cda21ef74fae5a6b467c762dc3b87e263c
Vladimir <vl.petrosyan94@gmail.com>
2025-04-30 14:19:13 +0400

    feat(api): Add new routes for historical dates and categories & project details

---
+++ src/app/api/v2/historical-dates/categories/route.tsx
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server';
+
+import { config } from '@/shared/lib/config/config';
+
+const handler = async (req: Request) => {
+  const { method } = req;
+
+  if (method === 'GET') {
+    try {
+      const url = `${config.domain}/mock/historical-dates.json`;
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      const categories = Object.keys(data).map((item: string) => item);
+
+      return NextResponse.json(categories, {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    } catch (error) {
+      return new Response(JSON.stringify(error), { status: 500 });
+    }
+  }
+
+  return new Response('Method Not Allowed', { status: 405 });
+};
+export { handler as GET, handler as POST };
