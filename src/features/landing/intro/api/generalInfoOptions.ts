637eef0bb27bce08cc7e2f7da352e7b204944681
Vladimir <vl.petrosyan94@gmail.com>
2025-04-30 19:34:56 +0400

    github best friend forever

--- src/features/landing/intro/api/generalInfoOptions.ts
+++ src/features/landing/intro/api/generalInfoOptions.ts
@@ -1,13 +1,15 @@
 import { queryOptions } from '@tanstack/react-query';
 
-import { config } from '@/shared/lib/config/config';
+// import { config } from '@/shared/lib/config/config';
 import { IProject } from '@/shared/types/team';
 
 export const generalInfoOptions = queryOptions<IProject>({
   queryKey: ['general-info'],
   queryFn: async () => {
     try {
-      const response = await fetch(`${config.apiUrl}/general-info`);
+      const response = await fetch(
+        `https://github.com/funnyMans/only-test/blob/main/public/mock/project.json`
+      );
       return response.json();
     } catch (error) {
       return JSON.stringify(error);
